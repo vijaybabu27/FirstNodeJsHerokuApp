939fcd76c84e775b7862bc6e896448bcce8c34f5
Vijayababu Venugopal <vijaybabu27@gmail.com>
2019-06-12 23:46:34 -0400

    Updated to hbs view engine ;

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index.hbs', { title: 'Express', msg: 'My First Application Deployed in Heroku' });
 });
 
 module.exports = router;
